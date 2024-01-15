6c5f26bb5c5a6753fad860c3c05ca17a514f7a7f
luckened <lucfmiranda@gmail.com>
2024-01-14 21:30:46 -0300

    feat: homepage content

--- src/data/index.js
+++ src/data/index.js
@@ -1,33 +1,30 @@
 export const JobData = [
   {
+    title: "Commit Jr",
+    description:
+      "Commit Jr. is a Junior Enterprise located on CEFET-MG made only by its students. There, I've gained experience in building a software factory from the ground up with my colleagues. I've had the opportunity to develop solutions for our clients and manage the company members, overseeing selection processes and activities to foster an excellent organizational climate for our team.",
+    image: "/commitjr_logo.jpeg",
+    link: "https://commitjr.com/",
+  },
+  {
     title: "studiosol",
     description:
       "At Studio Sol, I've had the opportunity to work at the best place for brazilian -and international- independent musicians to go: palcomp3.com. Refactored all frontend song listings to enhance user experience, Applied code splitting with webpack, reducing the PWA's JavaScript size, Implemented various refactors focusing on web performance, improving core web vitals scores such as LCP, FID, and CLS, setup and end-to-end tests with cypress.",
-    image: "/studiosol.png",
+    image: "/studio_sol_logo.jpeg",
     link: "https://www.studiosol.com.br/",
   },
   {
-    title: "studiosol12",
-    description: "Find in-depth information about Next.js features and API.",
-    image: "/studiosol.png",
-    link: "https://www.studiosol.com.br/",
+    title: "Buser",
+    description:
+      "Buser is changing the way travelers book bus trips. I've worked in the company's main PWA: the passenger application. Developed and maintained features for booking bus tickets, enhancing the user experience for over 5 million end-users, Implemented JSON-LD structured data as a Product type for SEO improvements on Google, built and maintained UI components for Andromeda, Buser's Design System, led the implementation of the first unit and integration tests in the company, fostering a culture of software testing through tech talks and code reviews, maintained Android & iOS mobile webview",
+    image: "/buserbrasil_logo.jpeg",
+    link: "https://www.buser.com.br/",
   },
   {
-    title: "studiosol121212",
-    description: "Find in-depth information about Next.js features and API.",
-    image: "/studiosol.png",
-    link: "https://www.studiosol.com.br/",
-  },
-  {
-    title: "studioso1212l",
-    description: "Find in-depth information about Next.js features and API.",
-    image: "/studiosol.png",
-    link: "https://www.studiosol.com.br/",
-  },
-  {
-    title: "stu2di2osol",
-    description: "Find in-depth information about Next.js features and API.",
-    image: "/studiosol.png",
-    link: "https://www.studiosol.com.br/",
+    title: "QuintoAndar",
+    description:
+      "The biggest PropTech in Latin America. I'm currently working on web platform team, building a solid platform for the company software developers focusing on front-end engineering. Led the Web Platform team, constructing a robust foundation for company developers with a focus on frontend engineering, maintained a developer portal with frontend application analytics using Backstage from Spotify, facilitating information access for over 800 tech professionals, spearheaded the creation of a frontend monorepo with Nx, hosting all company PWAs (50+), managed a monorepo containing key frontend libraries, packages, patterns, and similar products used by over 100 frontend engineers, designed, built, and maintained the experimentation platform enabling seamless A/B tests, feature flags, remote configuration, and more, achieved a 72% reduction in AWS's lambda@edge runtime, resulting in savings of up to $10,000 per month, decreased the company's main PWA CI times by 30%, achieved a 92% reduction in lambda@edge CI times",
+    image: "/quintoandar_com_br_logo.jpeg",
+    link: "https://www.quintoandar.com.br/",
   },
 ];
